154eddff3e0616b63c4ac3a5c1c5643d1e99c943
Leapwork Play <noreply+leapwork-play@leapwork.local>
2026-08-27 16:11:20 +0530

    File-Create-Delete added and working

--- Create-Delete all type of Assets/TC-29-File-Create-Delete.spec.ts
+++ Create-Delete all type of Assets/TC-29-File-Create-Delete.spec.ts
@@ -1,16 +1,21 @@
 import { leapwork } from "./leapwork";
 
-import { RandomTeam, teamName } from "@assets/Utilities/random-team";
-import { MicrosoftLogin } from "@assets/Utilities/Microsoft Login";
-import { DeleteCreateRenameTeam } from "@assets/Utilities/Delete-Create-Rename Team";
 import { CreateNewAsset } from "@assets/Utilities/Create New Asset";
 import { RenameAsset } from "@assets/Utilities/Rename Asset";
 import { DeleteAsset } from "@assets/Utilities/Delete Asset";
 import { DeleteTeam } from "@assets/Utilities/Delete Team";
+import { MicrosoftLogin } from "@assets/Utilities/Microsoft Login";
+import { RandomTeam, teamName } from "@assets/Utilities/random-team";
+import { DeleteCreateRenameTeam } from "@assets/Utilities/Delete-Create-Rename Team";
 
 leapwork.configuration({
-  enableSelfHeal: false,
-  timeoutMs: 5000
+  timeoutMs: Number(
+    leapwork.team.settings.get("timeoutMs")
+    ?? leapwork.workspace.settings.get("timeoutMs")
+  ) || 5000,
+  enableSelfHeal:
+    (leapwork.team.settings.get("enableSelfHeal")
+      ?? leapwork.workspace.settings.get("enableSelfHeal")) !== "false",
 });
 
 leapwork.variables.set("userId", "user_38");
@@ -19,12 +24,18 @@ leapwork.variables.set("assetType", "New file");
 leapwork.variables.set("assetName", "created");
 leapwork.variables.set("renamedAssetName", "renamed");
 
-// ai-studio-step-id: 3d2f8e33
+
+const lw__text = "abc";
+const lw__text2 = "def";
+
+// ai-studio-step-id: pw1vs03ju0
 await leapwork.step("Use test case: Microsoft Login", async () => {
     return await MicrosoftLogin();
 }, { action: "asset_reference", linkedAssetType: "test-case" });
 
-// ai-studio-step-id: 938bace8
+
+
+// ai-studio-step-id: pwyrpbaz00
 await leapwork.step("Use test case: Delete-Create-Rename Team", async () => {
     return await DeleteCreateRenameTeam();
 }, { action: "asset_reference", linkedAssetType: "test-case" });
