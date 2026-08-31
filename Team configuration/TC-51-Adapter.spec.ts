d25d3d8bdd9d71ca27df7173bb968a386421eab3
Leapwork Play <noreply+leapwork-play@leapwork.local>
2026-08-31 11:56:24 +0530

    Fixes in adapter TC - Similar to STG

--- Team configuration/TC-51-Adapter.spec.ts
+++ Team configuration/TC-51-Adapter.spec.ts
@@ -23,23 +23,26 @@ await leapwork.step("Use test case: Delete-Create-Rename Team", async () => {
 }, { action: "asset_reference", linkedAssetType: "test-case" });
 
 
-// ai-studio-step-id: pw1crr8k80
-await leapwork.step("Click the Create new button for \"${teamName}\"", async () => {
-    const createNewButton = page.getByRole('button', { name: `Create new in ${teamName}`, exact: true });
-    await expect(createNewButton).toHaveCount(1);
-    await createNewButton.click({ force: true });
+// ai-studio-step-id: pw41d9me00
+await leapwork.step("Click New team", async () => {
+    // Click "New team"
+     await page.locator(`div.explorer-item[title="${teamName}"]`).click();
 }, { action: "click" });
-
-// ai-studio-step-id: pwub1yxp00
-await leapwork.step("Click New typescript in the assets create new menu", async () => {
-    const loc = page.getByText('New typescript');
-    await loc.waitFor({ state: 'visible' });
+ 
+// ai-studio-step-id: pwcia05700
+await leapwork.step("Click Create new in the New team section", async () => {
+    // Click button "Create new in New team"
+    await page.getByRole('button', { name: `Create new in ${teamName}` }).click();
+}, { action: "click" });
+ 
+// ai-studio-step-id: pw1606r1e0
+await leapwork.step("Click New recording adapter in the context menu", async () => {
     // Click div
-    await page.getByText('New typescript').click();
+    await page.getByText('New recording adapter').click();
 }, { action: "click" });
-
+ 
 // ai-studio-step-id: pw1vp87ws0
-await leapwork.step("Click the Collapse chat button in Leapwork AI Studio", async () => {
+await leapwork.step("Click the Collapse chat button in Leapwork Play.", async () => {
     // Click button "Collapse chat"
     await page.getByRole('button', { name: 'Collapse chat' }).click();
 }, { action: "click" });
@@ -49,26 +52,16 @@ await leapwork.step("Double click the \"${teamName}\" folder in Leapwork AI Stud
     await page.locator('.explorer-list').getByText(teamName, { exact: true }).dblclick();
 });
 
-// ai-studio-step-id: pwo6pxhk00
-await leapwork.step("Select Team adapter", async () => {
-    const teamAdapter = page.getByRole('combobox', { name: 'Team adapter', exact: true });
-    await expect(teamAdapter).toHaveCount(1);
-    await teamAdapter.click({ force: true });
-}, { action: "click" });
-
-// ai-studio-step-id: pwza0gfb00
-await leapwork.step("Select \"New typescript (TS-1)\" from Team adapter dropdown", async () => {
+// ai-studio-step-id: pw1251nyp0
+await leapwork.step("Select \"New recording adapter (TS-1)\" from Team adapter", async () => {
     // Click combobox "Team adapter"
-    await page.locator('select').selectOption({ label: 'New typescript (TS-1)' });
-}, { action: "click" });
+    await page.getByLabel('Team adapter').selectOption({ label: "New recording adapter (TS-1)" });
+}, { action: "click", relativeXpath: "//*[@id=\"team-recording-adapter-select\"]" });
 
-// ai-studio-step-id: pw14cl4y20
-await leapwork.step("Validate the Team adapter is 'New typescript (TS-1)' on the Leapwork AI Studio page", async () => {
-    // Assert combobox "Team adapter" contains "New typescript (TS-1)"
-    await expect(page.getByLabel('Team adapter').locator('option:checked')).toHaveText('New typescript (TS-1)');
-}, { action: "validate" });
+// ai-studio-step-id: pw1lmdd880
+await leapwork.step("Validate the Team adapter is set to \"New recording adapter (TS-1)\" on Leapwork Play", async () => {
+    // Assert combobox "Team adapter" contains "New recording adapter (TS-1)"
+    await expect(page.getByLabel('Team adapter')).toContainText("New recording adapter (TS-1)");
+}, { action: "validate", relativeXpath: "//*[@id=\"team-recording-adapter-select\"]" });
 
-// ai-studio-step-id: 8814c963
-await leapwork.step("Use test case: Delete Team", async () => {
-    return await DeleteTeam();
-}, { action: "asset_reference", linkedAssetType: "test-case" });
+
